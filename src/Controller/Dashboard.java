5c5e84810b0cfd4c333a409f4589485c5f94edd3
rivinduc <trivinduchamath333@gmail.com>
2019-11-05 20:18:27 +0530

    Initial Commit

---
+++ src/Controller/Dashboard.java
@@ -0,0 +1,39 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class Dashboard {
+
+    public ImageView imgDeleteTimeTable;
+    @FXML
+    private ImageView imgManageStudent;
+
+    @FXML
+    private ImageView imgSearchStudent;
+
+    @FXML
+    private ImageView imgUpdateStudent;
+
+    @FXML
+    private ImageView imgDeleteStudent;
+
+    @FXML
+    private ImageView imgStudentTimeTable;
+
+    @FXML
+    private Label lblWelcome;
+
+    @FXML
+    void imgMouseExit(MouseEvent event) {
+
+    }
+
+    @FXML
+    void imgMouseOn(MouseEvent event) {
+
+    }
+
+}
---
+++ src/DBConnection/DBConnection.java
@@ -0,0 +1,41 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBConnection {
+    private static Connection connection;
+    public static Connection getConnection() throws Exception{
+
+        if (null==connection){
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/StudentManagement?" +
+                            "createDatabaseIfNotExist=true&allowMultiQueries=true",
+                    "root",
+                    "Password");
+            PreparedStatement pstm = connection.prepareStatement("SHOW TABLES");
+            ResultSet resultSet = pstm.executeQuery();
+            if (!resultSet.next()){
+                String sql = "create table student(\n" +
+                        " id varchar(19)primary key,\n" +
+                        "name varchar(30),\n" +
+                        " address varchar(30),\n" +
+                        "telephone int(10)\n" +
+                        ");\n" +
+                        "create table timeTable(\n" +
+                        " id varchar(19)primary key,\n" +
+                        " subject varchar(30),\n" +
+                        " lecturer varchar(30)\n" +
+                        ");\n" +
+                        "\n" +
+                        "\n" +
+                        "\n";
+                pstm = connection.prepareStatement(sql);
+                pstm.execute();
+            }
+        }
+        return connection;
+    }
+
+}
