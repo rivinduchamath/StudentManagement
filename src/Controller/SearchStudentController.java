fe46485651740ed4c2ec14df09502cbffe86f283
madushanka kumara <oska.coc@gmail.com>
2019-11-05 21:34:48 +0530

    Complete studentSearch.fxml

---
+++ src/Controller/SearchStudentController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableView;
+
+public class SearchStudentController {
+    public JFXTextField searchStudent;
+    public TableView tbl_student;
+
+    public void initialize() {
+
+    }
+}
