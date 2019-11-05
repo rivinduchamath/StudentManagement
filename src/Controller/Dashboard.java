21efb98091f7c4ec4dfebab79ddc3a9d5d069f25
sudharaka <subhy0422@gmail.com>
2019-11-05 23:33:51 +0530

    Create FXML

--- src/Controller/Dashboard.java
+++ src/Controller/Dashboard.java
@@ -134,7 +134,7 @@ public class Dashboard  implements Initializable {
                     root = FXMLLoader.load(this.getClass().getResource("#"));
                     break;
                 case "imgUpdateStudent":
-                    root = FXMLLoader.load(this.getClass().getResource("#"));
+                    root = FXMLLoader.load(this.getClass().getResource("UpdateForm.fxml"));
                     break;
                 case "imgDeleteStudent":
                     root = FXMLLoader.load(this.getClass().getResource("#"));
